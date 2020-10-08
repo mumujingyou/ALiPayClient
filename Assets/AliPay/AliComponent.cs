7990eeb91937d2b0d34129ba75e386d643289a5a
mumujingyou <2542472224@qq.com>
2020-10-08 23:44:30 +0800

    提交信息

---
+++ Assets/AliPay/AliComponent.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class AliComponent : MonoBehaviour {
+    //支付结果 通知哪个游戏物体
+     string aliSDKCallObjName = "AliComponent";
+    //支付结果 调用的方法名称
+     string aliSDKCallFuncName = "AliPayCallback";
+
+    //是java里的类，一些静态方法可以直接通过这个调用。
+    //androidjavaobject 调用的话，会生成一个对象，就和java里new一个对象一样，可以通过对象去调用里面的方法以及属性。
+    AndroidJavaClass javaClass;
+    AndroidJavaObject javaActive;
+
+    //"com.mafeng.alinewsdk.AliSDKActivity"是2018.11.01日更新的版本 对应安卓工程中的alinewsdk Module
+    //而"com.mafeng.aliopensdk.AliSDKActivity"旧版 对应安卓工程中的aliopensdk Module
+
+    string javaClassStr = "com.mafeng.alinewsdk.AliSDKActivity";
+    string javaActiveStr = "currentActivity";
+
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+        javaActive = new AndroidJavaObject(javaClassStr);
+#elif UNITY_IPHONE
+#endif
+
+    }
+
+
+
+    /// <summary>
+    /// 支付宝支付
+    /// </summary>
+    public void AliPay(string OrderInfo)
+    {
+
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+        object[] objs = new object[] { OrderInfo,aliSDKCallObjName, aliSDKCallFuncName };
+        javaActive.Call("AliPay", objs);
+#elif UNITY_IPHONE
+#endif
+
+    }
+
+    public Action<string> aliPayCallBack;
+
+    /// <summary>支付宝支付回调</summary>
+    public void AliPayCallback(string result)
+    {
+
+        aliPayCallBack(result);
+        //告诉服务器已经支付 等待返回结果
+        //再监听结果 进行发放奖励 实际上都是独立的
+        if (result == "支付成功")
+        {
+            Debug.Log("支付宝支付成功");
+        }
+        else
+        {
+            Debug.Log("支付宝支付失败");
+        }
+    }
+
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    TcpClient client;
+    Button payBtn;
+    Text PayResult1, PayResult2;
+    AliComponent Ali;
+
+    void Start()
+    {
+        Ali= GameObject.Find("AliComponent").GetComponent<AliComponent>();
+
+        client = new TcpClient();
+        Connect();
+
+        payBtn = transform.Find("Panel/AliPayButton").GetComponent<Button>();
+        payBtn.onClick.AddListener(Send);
+        PayResult1=transform.Find("Panel/PayResult1").GetComponent<Text>();
+        PayResult2 = transform.Find("Panel/PayResult2").GetComponent<Text>();
+
+        Ali.aliPayCallBack += GetAliPayResult;
+
+    }
+
+    private void GetAliPayResult(string obj)
+    {
+        PayResult1.text = $"同步通知:{obj}";
+    }
+
+    async void Connect() {
+        try
+        {
+            await client.ConnectAsync("193.112.44.199",7577);
+            Receive();
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"连接异常:{e.Message}");
+        }
+    }
+
+    private async void Receive()
+    {
+        try
+        {
+            if (client.Connected)
+            {
+                byte[] buffer = new byte[1024];
+                int length= await client.GetStream().ReadAsync(buffer,0, buffer.Length);
+                string s = Encoding.UTF8.GetString(buffer,0,length);
+                if (s!="error")
+                {
+                    if (s!="支付成功"&&s!="支付失败")
+                    {
+                        Ali.AliPay(s);
+                    }
+                    else
+                    {
+                        PayResult2.text = $"异步通知结果:{s}";
+                    }
+                   
+                }
+                else
+                {
+                    Debug.LogError("服务器请求预支付单号时候出现异常");
+                }
+                Receive();
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"接收异常:{e.Message}");
+           
+        }
+    }
+
+    string sendStr = "pay";
+    private async void Send() {
+        try
+        {
+            if (client.Connected)
+            {
+                PayResult1.text = "同步通知:";
+                PayResult2.text = "异步通知结果:";
+
+                byte[] data = Encoding.UTF8.GetBytes(sendStr);
+                await client.GetStream().WriteAsync(data, 0, data.Length);
+                Debug.Log("发送成功,请求支付参数");
+            }
+        
+        }
+        catch (Exception e)
+        {
+
+            Debug.LogError($"发送异常:{e.Message}");
+        }
+      
+    }
+}
